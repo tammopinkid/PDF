ecc1440dc18d131d3735f05d246497a882c8c3ca
iherem <kittisak.24@mail.kmutt.ac.th>
2017-07-05 18:12:38 +0700

    add compile type Option

--- src/models/FilterType.model.js
+++ src/models/FilterType.model.js
@@ -1,5 +1,8 @@
 import Textbox from '../types/Textbox.type'
+import Option from '../types/Option.type'
 import Generator from './Generator.model'
+import { ignoreEvent } from '../../config/eventIgnore.json'
+import _ from 'lodash'
 const filter = (jsonObject) => {
   let events = jsonObject.events
   let data = []
@@ -7,10 +10,23 @@ const filter = (jsonObject) => {
     events.forEach( item => {
       const key = item.fieldId
       const componentType = JSON.parse(item.payload).params.componentType
-      switch (componentType){
-        case 'Textbox':{
-          data.push(Textbox.compile(item))
-          break
+      //console.log(key)
+      let isIgnore = false
+      _.map(ignoreEvent, item => {
+        if (key === item){
+          isIgnore = true
+        }
+      })
+      if (isIgnore === false){
+        switch (componentType){
+          case 'Textbox':{
+            data.push(Textbox.compile(item))
+            break
+          }
+          case 'Option':{
+            data.push(Option.compile(item))
+            break
+          }
         }
       }
     })
--- src/module.js
+++ src/module.js
@@ -1,4 +1,16 @@
 import events from '../test/Events-JaFLVAPz.json'
 import Filter from './models/FilterType.model'
+import _ from 'lodash'
+import fs from 'fs'
 const data = Filter.filter(events)
 //console.log(Event)
+// console.log(events)
+// _.map(events.events, item => {
+//   console.log(JSON.parse(item.payload))
+// })
+// fs.writeFile('./output/eiei.json', JSON.stringify(events,null,2), 'utf-8', function(err) {
+//   if (err){
+//     throw err
+//   }
+//   console.log('write json file successfully.')
+// })
