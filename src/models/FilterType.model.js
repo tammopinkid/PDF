f9f278dd126a9463dec2ffeb86cccae84771167f
tammopinkid <benjarat.saeang@gmail.com>
2017-07-05 18:46:18 +0700

    update componentType : Option finishgit add .

--- src/models/FilterType.model.js
+++ src/models/FilterType.model.js
@@ -5,6 +5,9 @@ import Checkbox from '../types/Checkbox.type';
 import Yesnolist from '../types/Yesnolist.type';
 import InfiniteDropdown from '../types/InfiniteDropdown.type';
 import Picker from '../types/Picker';
+import { ignoreList } from '../../config/eventIgnore.json';
+import _ from 'lodash';
+//console.log(ignoreList);
 const filter = jsonObject => {
   let events = jsonObject.events;
   let data = [];
@@ -15,42 +18,51 @@ const filter = jsonObject => {
       const sequenceId = item.sequenceId;
       const componentType = JSON.parse(item.payload).params.componentType;
       //i++
-      switch (componentType) {
-        case 'Textbox': {
-          data.push(Textbox.compile(item));
-          break;
+      let isIgnore = false;
+      _.map(ignoreList, item => {
+        if (key === item) {
+          isIgnore = true;
         }
-        // case 'Option': {
-        //   if (Option.compile(item)) {
-        //     data.push(Option.compile(item));
-        //     //console.log('textbox = ', componentType);
-        //   }
-        //   break;
-        // }
-        case 'Checkbox': {
-          data.push(Checkbox.compile(item));
-          break;
-        }
-        //case 'YesNoList': {
-        //data.push(Yesnolist.compile(item))
+      });
+      if (isIgnore === false) {
+        switch (componentType) {
+          case 'Textbox': {
+            data.push(Textbox.compile(item));
+            break;
+          }
+          case 'Option': {
+            if (Option.compile(item)) {
+              data.push(Option.compile(item));
+              //console.log('textbox = ', componentType);
+            }
+            break;
+          }
+          case 'Checkbox': {
+            data.push(Checkbox.compile(item));
+            break;
+          }
+          //case 'YesNoList': {
+          //data.push(Yesnolist.compile(item))
 
-        //break
-        //}
-        case 'InfiniteDropdown': {
-          data.push(InfiniteDropdown.compile(item));
-          break;
-        }
-        case 'Picker': {
-          data.push(Picker.compile(item));
-          break;
+          //break
+          //}
+          case 'InfiniteDropdown': {
+            data.push(InfiniteDropdown.compile(item));
+            break;
+          }
+          case 'Picker': {
+            data.push(Picker.compile(item));
+            break;
+          }
         }
       }
+
       // if (sequenceId === 411) {
       //   console.log(item.payload);
       // }
     });
   }
-  console.log(data);
+  //console.log(data);
   //console.log('i = ',i)
   Generator.generate(data);
 };
--- src/models/Generator.model.js
+++ src/models/Generator.model.js
@@ -1,50 +1,6 @@
-// import Positions from '../../config/position.json'
-// import fs from 'fs'
-
-// const toJSONFile = (data)=>{
-//   fs.writeFile('./output/out.json', JSON.stringify(data,null,4), 'utf-8', function(err) {
-//     if (err){
-//       throw err
-//     }
-//     console.log('write json file successfully.')
-//   })
-// }
-
-// const generate = (dataObject) => {
-//   let results = []
-//   let result = {}
-//   let page = {}
-//   let current_page = 0
-//   Positions.forEach(position => {
-//     if (current_page !== position.p){
-//       results = []
-//       current_page = position.p
-//     }
-//     dataObject.forEach(data => {
-//       result = {}
-//       if (position.key === Object.keys(data)[0]){
-//         result.text = data[Object.keys(data)]
-//         result.x = position.x
-//         result.y = position.y
-//         result.optlist = 'abcd'
-//         results.push(result)
-//       }
-//     })
-//     page[current_page] = results
-//   })
-//   //console.log(page)
-//   toJSONFile(page)
-//   //console.log(__dirname)
-//   //console.log(fs)
-//   return dataObject
-// }
-
-// export default {
-//   generate
-// }
-
 import Positions from '../../config/position.json';
 import fs from 'fs';
+import _ from 'lodash';
 
 const toJSONFile = data => {
   fs.writeFile(
@@ -76,13 +32,26 @@ const generate = dataObject => {
       current_page = position.p;
     }
     dataObject.forEach(data => {
-      result = {};
-      if (position.key === Object.keys(data)[0]) {
-        result.text = data[Object.keys(data)];
-        result.x = position.x;
-        result.y = position.y;
-        result.optlist = 'abcd';
-        results.push(result);
+      if (_.isArray(data)) {
+        _.map(data, item => {
+          result = {};
+          if (position.key === Object.keys(item)[0]) {
+            result.text = item[Object.keys(item)];
+            result.x = position.x;
+            result.y = position.y;
+            result.optlist = 'abcd';
+            results.push(result);
+          }
+        });
+      } else {
+        result = {};
+        if (position.key === Object.keys(data)[0]) {
+          result.text = data[Object.keys(data)];
+          result.x = position.x;
+          result.y = position.y;
+          result.optlist = 'abcd';
+          results.push(result);
+        }
       }
     });
     page[current_page] = results;
